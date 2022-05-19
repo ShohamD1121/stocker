b01f6494fb45c5e67ed1602d21161c7489ae7028
shohamD1121 <shohamdouek@gmail.com>
2022-05-19 20:04:47 +0300

    updateing domain

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Features from "./Pages/Features/Features";
 import FAQ from "./Pages/FAQ/FAQ";
 import ContactUs from "./Pages/Contact Us/ContactUs";
