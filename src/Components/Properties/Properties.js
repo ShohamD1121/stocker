42578d16b1507885a9728a72b264181b31c41bc0
shohamD1121 <shohamdouek@gmail.com>
2022-05-28 18:49:33 +0300

    updating google analytics

--- src/Components/Properties/Properties.js
+++ src/Components/Properties/Properties.js
@@ -1,56 +1,73 @@
 import React, { Fragment } from "react";
-import { Header, Wrapper,SecondWrapper, Title, Description, SmallTitle, SmallDescription } from "./Propeties.elements";
+import {
+  Header,
+  Wrapper,
+  SecondWrapper,
+  Title,
+  Description,
+  SmallTitle,
+  SmallDescription,
+} from "./Propeties.elements";
 import { FaEye, FaDatabase, FaCode, FaUserFriends } from "react-icons/fa";
 import { Container } from "../../globalStyles";
 
 const Properties = ({ BgColor, TextColor, Text }) => {
-
-
   return (
     <Fragment>
       <Container>
-        <Wrapper >
+        <Wrapper>
           <Header style={{ background: BgColor[0], color: TextColor[0] }}>
-            <FaEye style={{ fontSize: "20px", margin : "auto"}} />
+            <FaEye style={{ fontSize: "20px", margin: "auto" }} />
             {Text}
           </Header>
           <Title>The world's first personal AI stock screener</Title>
           <Description>
-            Instead of wasting hours on finding the right stocks that fit your
-            personal investing style, Stocker's powerful AI algorithm picks
-            quickly the best stocks for you in addition to realtime stock market
-            financials, News and Analysis.
+            The key to successful investing is knowing which stocks to buy.
+            Stocker is an AI powered stock picking mobile app. It uses a
+            powerful algorithm to pick the best stocks for the user based on his
+            investing style and provides realtime financials, news, analysis.
           </Description>
         </Wrapper>
-        <SecondWrapper >
-          <Wrapper >
+        <SecondWrapper>
+          <Wrapper>
             <Header style={{ background: BgColor[0], color: TextColor[0] }}>
               <FaDatabase style={{ fontSize: "20px" }} />
             </Header>
-            <SmallTitle>Real-Time<br/>stocks data</SmallTitle>
+            <SmallTitle>
+              Real-Time
+              <br />
+              stocks data
+            </SmallTitle>
             <SmallDescription>
-              Collect realtime financial stock data on over 10,0000 tickers all
-              over the US stock market.
+              Collect realtime financial stock data on over 10,000 tickers across the US stock market.
             </SmallDescription>
           </Wrapper>
-          <Wrapper >
+          <Wrapper>
             <Header style={{ background: BgColor[1], color: TextColor[1] }}>
               <FaCode style={{ fontSize: "20px" }} />
             </Header>
-            <SmallTitle>Powerful AI<br/>Algorithm</SmallTitle>
+            <SmallTitle>
+              Powerful AI
+              <br />
+              Algorithm
+            </SmallTitle>
             <SmallDescription>
-              Our advanced powerful AI algorithmwill learn your investing style
+              Our advanced powerful AI algorithm will learn your investing style
               based on the stocks you like in order to pick the best stocks for
               you.
             </SmallDescription>
           </Wrapper>
-          <Wrapper >
+          <Wrapper>
             <Header style={{ background: BgColor[2], color: TextColor[2] }}>
               <FaUserFriends style={{ fontSize: "20px" }} />
             </Header>
-            <SmallTitle>User friendly<br/>experience</SmallTitle>
+            <SmallTitle>
+              User friendly
+              <br />
+              experience
+            </SmallTitle>
             <SmallDescription>
-              User experience is on the top of our values. Our goal is to make
+              User experience upon the top of our values. Our goal is to make
               stock investing in the most friendly and comfortable way.
             </SmallDescription>
           </Wrapper>
